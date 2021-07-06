a5c33d14714089576049d787e1fdf622d4fa5a2a
공민철 <68903200+kongs510@users.noreply.github.com>
2021-07-06 17:09:32 +0900

    프론트엔드의 이해-first commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+// import * as math from "./math.js"
+import "./style.css"
+
+ // 3
+// console.log(math.sum(1, 2));
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path")
+const MyPlugin = require("./myplugin")
+const webpack = require('webpack');
+const banner = require("./banner.js")
+
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+  plugins: [
+    new webpack.BannerPlugin(banner)
+  ],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ["style-loader", "css-loader"], // style-loader를 앞에 추가한다
+    },{
+        test: /\.png$/, // .png 확장자로 마치는 모든 파일
+        loader: "file-loader", // 파일 로더를 적용한다
+        options: {
+          publicPath: "./dist/", // prefix를 아웃풋 경로로 지정
+          name: "[name].[ext]?[hash]", // 파일명 형식
+        },
+      },
+    ],
+  }
+}
